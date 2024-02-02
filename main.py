96875362ead7f5db9e6838a895519cb9ab35a9ce
Gibran <gibranfahad101@gmail.com>
2024-02-01 19:52:48 -0600

    Main widgets added

--- main.py
+++ main.py
@@ -8,11 +8,34 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
-# window.after(1000, )
-canvas = Canvas(width=200, height=200, highlightthickness=0)
+window.config(padx=50, pady=50, bg="white")
+canvas = Canvas(width=200, height=200, highlightthickness=0, bg="white")
 lock = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock)
-canvas.grid(column=1, row=1)
+canvas.grid(row=0, column=1)
+
+website_label = Label(text="Website:", bg="white")
+website_label.grid(row=1, column=0)
+
+email_label = Label(text="Email/ Username:", bg="white")
+email_label.grid(row=2, column=0)
+
+password_label = Label(text="Password:", bg="white")
+password_label.grid(row=3, column=0)
+
+website_entry = Entry(width=43)
+website_entry.grid(row=1, column=1, columnspan=2)
+
+email_entry = Entry(width=43)
+email_entry.grid(row=2, column=1, columnspan=2)
+
+password_entry = Entry(width=33)
+password_entry.grid(row=3, column=1)
+
+password_generator = Button(text="Generate", bg="white", highlightthickness=0)
+password_generator.grid(row=3, column=2)
+
+add_password = Button(text="Add", width=36, bg="white", highlightthickness=0)
+add_password.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
