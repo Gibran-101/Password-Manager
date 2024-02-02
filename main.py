4a8cd87a1081a4f2137a9f2ebdd5b28919494865
Gibran <gibranfahad101@gmail.com>
2024-02-01 21:41:20 -0600

    Created  method to write the user credentials to data.txt

--- main.py
+++ main.py
@@ -14,6 +14,18 @@ lock = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock)
 canvas.grid(row=0, column=1)
 
+def save():
+    web_text = website_entry.get()
+    mail_text = email_entry.get()
+    password_text = password_entry.get()
+    with open("data.txt", mode='a') as data:
+        data.write(f"Website: {web_text}, Email Address: {mail_text}, Password: {password_text}\n")
+    website_entry.delete(0, END)
+    email_entry.delete(0, END)
+    email_entry.insert(0, "username@gmail.com")
+    password_entry.delete(0, END)
+
+
 website_label = Label(text="Website:", bg="white")
 website_label.grid(row=1, column=0)
 
@@ -31,13 +43,13 @@ email_entry = Entry(width=43)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, "username@gmail.com")
 
-password_entry = Entry(width=33)
+password_entry = Entry(width=33, show="*")
 password_entry.grid(row=3, column=1)
 
 password_generator = Button(text="Generate", bg="white", highlightthickness=0)
 password_generator.grid(row=3, column=2)
 
-add_password = Button(text="Add", width=36, bg="white", highlightthickness=0)
+add_password = Button(text="Add", width=36, bg="white", highlightthickness=0, command=save)
 add_password.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
