07e7c9d1f1c6d48710d79124960993c0115a1808
Gibran <gibranfahad101@gmail.com>
2024-02-02 22:48:32 -0600

    Added Password Generator code

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+password_list = []
+
+for char in range(nr_letters):
+  password_list.append(random.choice(letters))
+
+for char in range(nr_symbols):
+  password_list += random.choice(symbols)
+
+for char in range(nr_numbers):
+  password_list += random.choice(numbers)
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-
-# ---------------------------- UI SETUP ------------------------------- #
-
-window = Tk()
-window.title("Password Manager")
-window.config(padx=50, pady=50, bg="white")
-canvas = Canvas(width=200, height=200, highlightthickness=0, bg="white")
-lock = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=lock)
-canvas.grid(row=0, column=1)
 
 
 def clear_canvas():
@@ -40,6 +58,16 @@ def save():
             clear_canvas()
     else:
         clear_canvas()
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50, bg="white")
+canvas = Canvas(width=200, height=200, highlightthickness=0, bg="white")
+lock = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock)
+canvas.grid(row=0, column=1)
 
 
 website_label = Label(text="Website:", bg="white")
